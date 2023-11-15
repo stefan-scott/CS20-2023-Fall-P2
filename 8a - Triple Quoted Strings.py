33961e3b23089018f0b8656dc87d7b6ad1fbaf66
stefan-scott <scottst@spsd.sk.ca>
2023-11-15 10:05:14 -0600

    Create 8a - Triple Quoted Strings.py

---
+++ 8a - Triple Quoted Strings.py
@@ -0,0 +1,20 @@
+# Triple Quoted Strings
+# Mr. Scott
+# Nov 8, 2023
+# ', ", ''', """
+
+#ways we can define strings - SINGLE QUOTES
+single_double = "Here's a string."
+single_single = 'Here is a "string."'
+
+#TRIPLE-QUOTED STRINGS
+triple_single = ''' Here's a "String" '''
+triple_double = """
+We can span
+some lines this
+way """
+
+#ESCAPE CHARACTERS 
+escape_string = "\t Test \n That\'s it."
+print(escape_string)  #\t → TAB  \n →Newline   \' → ' 
+
