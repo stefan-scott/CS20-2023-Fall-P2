cf334039a0e8c6071fd54db646bc538ec8065cb5
stefan-scott <scottst@spsd.sk.ca>
2023-12-04 10:09:51 -0600

    WIP

---
+++ 17 - Turtle Racing v1.py
@@ -0,0 +1,22 @@
+# Turtle Racing v1
+# Mr. Scott
+# Dec 4, 2023
+
+import random, turtle
+
+# RANDOM:  random.randint(low,high)  random.uniform(low,high) Float,   random.choice(list)
+
+#Set up window and some turtles
+w = turtle.Screen()
+
+lance = turtle.Turtle()
+andy = turtle.Turtle()
+lance.shape("turtle")
+andy.shape("turtle")
+lance.color("blue")
+andy.color("red")
+
+
+
+
+w.exitonclick()
